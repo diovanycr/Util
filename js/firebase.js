41c9d6b035d2d1f22ea9367413ef1eea8b1075e8
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-26 17:45:00 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,9 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "1:61793341031:web:572601dfa18fbf88248ec2",
+  apiKey: "AIzaSyB3nb-CDShDxKnN-naZdLWlyaXZfI6wvmY",
   authDomain: "respostas-automaticas-35aea.firebaseapp.com",
   projectId: "respostas-automaticas-35aea",
+  storageBucket: "respostas-automaticas-35aea.appspot.com",
+  messagingSenderId: "61793341031",
+  appId: "1:61793341031:web:572601dfa18fbf88248ec2"
 };
 
 export const app = initializeApp(firebaseConfig);
