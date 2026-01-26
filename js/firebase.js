517a67801446b7baf6c0211d47815181558f20e3
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-26 16:45:09 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "1:61793341031:web:572601dfa18fbf88248ec2",
+  authDomain: "respostas-automaticas-35aea.firebaseapp.com",
+  projectId: "respostas-automaticas-35aea",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
