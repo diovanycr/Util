fa92a8e74a36e7486e1634f3d411985fdbeeb234
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-26 16:45:39 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,29 @@
+import { auth } from './firebase.js';
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { loadAdmin } from './admin.js';
+import { loadUser } from './user.js';
+
+const el = id => document.getElementById(id);
+
+export function initAuth(){
+  el('btnLogin').onclick = async () => {
+    const user = el('loginUser').value;
+    const pass = el('loginPass').value;
+    await signInWithEmailAndPassword(auth, user, pass);
+  };
+
+  el('btnLogout').onclick = () => signOut(auth);
+
+  onAuthStateChanged(auth, user => {
+    if(!user) return;
+    el('loginBox').classList.add('hidden');
+    el('app').classList.remove('hidden');
+    el('loggedUser').textContent = user.email;
+
+    user.email.includes('admin')
+      ? loadAdmin()
+      : loadUser(user.uid);
+  });
+}
 
