6d4bb666cad23f1864d43d05cfd3afa3429c6a03
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-27 09:54:47 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
+// js/app.js
+
 import { initAuth } from './auth.js';
-import { initMessages } from './messages.js';
 import { initAdmin } from './admin.js';
-import { initUser } from './user.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initAuth();
-  initMessages();
   initAdmin();
-  initUser();
 });
