4f564eaa0ff92551bbf98c59c3c707135c7b37ae
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-27 10:12:15 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -14,8 +14,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
