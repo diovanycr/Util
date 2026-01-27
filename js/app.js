b5cbaaf7bb045b997f3cb4edefee0af6182a3620
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-27 11:32:32 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
 import { initAuth } from './auth.js';
-import { initAdmin } from './admin.js';
-import { initMessages } from './messages.js';
-import { closeModal } from './modal.js';
+import { initModalListeners } from './modal.js';
+import { initAdminActions } from './admin.js';
 
-// Inicializa o sistema de autenticação assim que a página carregar
 document.addEventListener('DOMContentLoaded', () => {
-    initAuth();
-    initAdmin();
-    // initMessages é chamado dentro do auth.js quando o login é confirmado
-    
-    // Configura o botão de fechar do modal se ele existir
-    const closeBtn = document.querySelector('.modal-close');
-    if (closeBtn) {
-        closeBtn.onclick = closeModal;
-    }
+  initModalListeners();
+  initAuth();
+  initAdminActions();
 });
