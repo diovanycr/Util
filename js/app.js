a3ff02e53c03a7d41693f31f0e7632c574428c8e
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-27 09:35:46 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-import { doLogin, doLogout } from './auth.js';
+import { initAuth } from './auth.js';
+import { initMessages } from './messages.js';
+import { initAdmin } from './admin.js';
+import { initUser } from './user.js';
 
-btnLogin.onclick = doLogin;
-btnLogout.onclick = doLogout;
+document.addEventListener('DOMContentLoaded', () => {
+  initAuth();
+  initMessages();
+  initAdmin();
+  initUser();
+});
