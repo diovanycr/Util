9faf3eece213c652998bb761ce727a638055226d
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-27 09:27:23 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,27 @@
-export function toast(msg){
-  const t = document.getElementById('toast');
-  t.textContent = msg;
-  t.style.display='block';
-  setTimeout(()=>t.style.display='none',2000);
+let confirmCallback = null;
+
+export function showModal(message){
+  const overlay = document.getElementById('modalOverlay');
+  document.getElementById('modalMessage').innerText = message;
+  overlay.style.display = 'flex';
 }
 
+export function closeModal(){
+  document.getElementById('modalOverlay').style.display = 'none';
+}
+
+export function openConfirmModal(cb){
+  confirmCallback = cb;
+  document.getElementById('confirmModal').classList.remove('hidden');
+}
+
+document.getElementById('modalCancel')?.addEventListener('click',()=>{
+  confirmCallback = null;
+  document.getElementById('confirmModal').classList.add('hidden');
+});
+
+document.getElementById('modalConfirm')?.addEventListener('click', async ()=>{
+  if(confirmCallback) await confirmCallback();
+  confirmCallback = null;
+  document.getElementById('confirmModal').classList.add('hidden');
+});
