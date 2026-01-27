80c232f3b600273b2603eed9ef3549ede13f5aa5
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-01-27 12:03:57 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 import { initAuth } from './auth.js';
 import { initModalListeners } from './modal.js';
-import { initAdminActions } from './admin.js';
+import { initAdminActions } from './admin.js'; // Importante!
 
 document.addEventListener('DOMContentLoaded', () => {
-  initModalListeners();
-  initAuth();
-  initAdminActions();
+    initModalListeners();
+    initAuth();
+    initAdminActions(); // Isso ativa o botão "Criar Usuário"
 });
