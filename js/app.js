a4b237fe32ec4b1f197f66a0591782ccd709a5d2
Diovany Rodrigues <37191875+diovanycr@users.noreply.github.com>
2026-02-10 20:11:59 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ import { initAuth } from './auth.js';
 import { initModalListeners } from './modal.js';
 import { initAdminActions } from './admin.js';
 import { initTabs } from './tabs.js';
+import { initTheme } from './theme.js';
+import { initShortcuts } from './shortcuts.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    initTheme();
+    initShortcuts();
     initModalListeners();
     initAuth();
     initAdminActions();
